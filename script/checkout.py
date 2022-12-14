2721364267f89994b089c8dbbfe10fe7998bf046
Nikita Prokopov <niki@tonsky.me>
2022-12-14 02:31:16 +0100

    m109-664500fa93

--- script/checkout.py
+++ script/checkout.py
@@ -51,9 +51,9 @@ def main():
   if 'windows' == common.system():
     env = os.environ.copy()
     env['PYTHONHTTPSVERIFY']='0'
-    subprocess.check_call(["python", "tools/git-sync-deps"], env=env)
+    subprocess.check_call(["python3", "tools/git-sync-deps"], env=env)
   else:
-    subprocess.check_call(["python2", "tools/git-sync-deps"])
+    subprocess.check_call(["python3", "tools/git-sync-deps"])
 
   return 0
 
