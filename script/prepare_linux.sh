ac7b5a14b700f48152f8cada160f19ad7e706af9
Nikita Prokopov <niki@tonsky.me>
2025-11-11 17:14:31 +0100

    Try enabling linux arm64 back #8

--- script/prepare_linux.sh
+++ script/prepare_linux.sh
@@ -7,7 +7,7 @@ add-apt-repository ppa:ubuntu-toolchain-r/test -y
 apt-get update -y
 apt-get install build-essential software-properties-common -y
 apt-get update
-apt-get install gcc-10 g++-10 -y
+apt-get install gcc-10 g++-10 gcc-10-aarch64-linux-gnu g++-10-aarch64-linux-gnu -y
 update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-10 60 --slave /usr/bin/g++ g++ /usr/bin/g++-10
 update-alternatives --config gcc 
 
