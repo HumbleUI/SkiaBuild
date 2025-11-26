8cd7768c6e142fe19e1b50bb4cd20175f3b350e0
vladimir.samsonov <vladimir.samsonov@devexpress.com>
2025-11-26 19:08:14 +0400

    m119-fcb55886b9

--- script/checkout.py
+++ script/checkout.py
@@ -8,10 +8,6 @@ def main():
   parser = common.create_parser(True)
   args = parser.parse_args()
 
-  # Clone depot_tools
-  if not os.path.exists("depot_tools"):
-    subprocess.check_call(["git", "clone", "https://chromium.googlesource.com/chromium/tools/depot_tools.git", "depot_tools"])
-
   # Clone Skia
   match = re.match('(m\\d+)(?:-([0-9a-f]+)(?:-([1-9][0-9]*))?)?', args.version)
   if not match:
@@ -48,12 +44,13 @@ def main():
     subprocess.check_call(["git", "apply", str(x)])
 
   # git deps
+  env = os.environ.copy()
   if 'windows' == common.system():
-    env = os.environ.copy()
     env['PYTHONHTTPSVERIFY']='0'
-    subprocess.check_call(["python3", "tools/git-sync-deps"], env=env)
-  else:
-    subprocess.check_call(["python3", "tools/git-sync-deps"])
+
+  subprocess.check_call(["python3", "tools/git-sync-deps"], env=env)
+  subprocess.check_call(["python3", "bin/fetch-gn"], env=env)
+  subprocess.check_call(["python3", "bin/fetch-ninja"], env=env)
 
   return 0
 
