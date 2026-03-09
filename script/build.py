d1647c2d03a51f5916e82371f8d5d123a8f381ce
vladimir.samsonov <vladimir.samsonov@devexpress.com>
2026-03-09 20:00:49 +0400

    Add runtime control for DirectWrite system font rendering params on Windows

--- script/build.py
+++ script/build.py
@@ -70,7 +70,7 @@ def main():
       'skia_use_system_freetype2=false',
       # 'skia_use_angle=true',
       'skia_use_direct3d=true',
-      #'extra_cflags=["-DSK_FONT_HOST_USE_SYSTEM_SETTINGS"]',
+      'extra_cflags=["-DSK_FONT_HOST_USE_SYSTEM_SETTINGS"]',
     ]
   elif 'android' == system:
     args += [
